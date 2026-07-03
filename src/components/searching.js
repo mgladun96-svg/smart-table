107c3e02569a07a216d8379ea43abc1500b558e7
Marina Gladun <mgladun96@gmail.com>
2026-07-03 16:31:03 +0300

    initial commit

--- src/components/searching.js
+++ src/components/searching.js
@@ -2,10 +2,26 @@ import {rules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
+  // @todo: #5.1 — настроить компаратор
+  const comparator = createComparison({
+    rules: {
+      ...rules.skipEmptyTargetValues,
+      ...rules.searchMultipleFields(
+        searchField,
+        ['date', 'customer', 'seller'],
+        false
+      )
     }
+  });
+
+  return (data, state, action) => {
+    // @todo: #5.2 — применить компаратор
+    const searchQuery = state[searchField] || '';
+
+    if (!searchQuery) {
+      return data;
+    }
+
+    return data.filter(item => comparator.compare(item, searchQuery));
+  };
 }
